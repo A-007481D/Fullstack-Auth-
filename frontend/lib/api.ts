d2f124f2de65456519c74bee3b8c27ed5c0a7f95
A-007481D <labidabdelmalek@gmail.com>
2026-08-21 02:59:14 +0100

    fix(frontend): add withCredentials to force cloud shell cookies

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -19,6 +19,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: '/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     Accept:         'application/json',
